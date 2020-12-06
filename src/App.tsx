5ef0c821ba7d9825b6cd065b96ffd616a9ef7db6
Irie Aoi <eai@mizle.net>
2020-12-06 14:18:59 +0900

    レベルを指定できるようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import React, { useState, useEffect } from "react";
-import Select, { components } from "react-select";
+import Select, { components, createFilter } from "react-select";
 import crs from "./crs.json";
 import "./App.css";
 
-function App() {
+function App(): JSX.Element {
     const [selectedOption, setSelectedOption] = useState<
         null | undefined | string
     >(null);
+    const [inputText, setInputText] = useState<null | string>(null);
 
     useEffect(() => {
         if (typeof selectedOption !== "string") {
             return;
         }
-        window.open(
-            `https://www.dododex.com/taming/${selectedOption}`,
-            "_blank"
-        );
+        const level = inputText?.split(",")[1];
+        const dododex = "https://www.dododex.com/";
+
+        const url = level
+            ? dododex + "taming/" + selectedOption + `#level=${level}`
+            : dododex + "taming/" + selectedOption;
+        window.open(url, "_blank");
     }, [selectedOption]);
 
     const { Option } = components;
@@ -39,8 +43,19 @@ function App() {
                 autoFocus={true}
                 options={crs}
                 components={{ Option: IconOption }}
+                onInputChange={(input) => {
+                    if (input) setInputText(input);
+                }}
                 onChange={(option) => {
                     setSelectedOption(option?.value);
+                }}
+                theme={(theme) => ({
+                    ...theme,
+                    borderRadius: 0,
+                })}
+                filterOption={(option, rawInput) => {
+                    const creature = rawInput.toLowerCase().split(",")[0];
+                    return option.value.includes(creature);
                 }}
             />
         </div>
